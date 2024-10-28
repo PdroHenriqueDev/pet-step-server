321cdaa249eb20fdbd19ea86129a38d561080b2b
PdroHenriqueDev <pedrohenriquedev28@gmail.com>
2024-10-28 15:57:13 -0300

    refactor: verify if dw has account before online

--- src/repositories/dogWalkerRepository.ts
+++ src/repositories/dogWalkerRepository.ts
@@ -156,6 +156,13 @@ class DogWalkerRepository {
 
       const {stripeAccountId, bank} = dogWalkerExists;
 
+      if (!stripeAccountId) {
+        return {
+          status: 400,
+          data: 'Você precisa adicionar uma conta bancária para que possa receber os valores dos passeios',
+        };
+      }
+
       if (!bank?.bankDocumentVerified) {
         const status = await StripeUtils.checkDocumentStatus(stripeAccountId);
 
