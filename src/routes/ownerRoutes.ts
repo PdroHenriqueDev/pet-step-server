34c5fe1b1cd38ea40e269d7672cd9c28bf93034a
PdroHenriqueDev <pedrohenriquedev28@gmail.com>
2024-11-01 18:30:27 -0300

    feat: firebase token when need to renew

--- src/routes/ownerRoutes.ts
+++ src/routes/ownerRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import Owner from '../controllers/ownerController';
+import {authenticateToken} from '../middleware/authenticateToken';
 
 const router = express.Router();
 
 router.post('/', Owner.store);
 
 router.get('/payments/:id', Owner.payments);
-router.get('/:id', Owner.findById);
+router.get('/:id', authenticateToken, Owner.findById);
 
 router.put('/:id/defaultPayment', Owner.updateDefaultPaymentMethod);
 
